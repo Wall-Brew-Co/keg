8e28a602482c3aaff0e290c6e86c0fedc96977c2
nnichols <nichols1991@gmail.com>
2020-10-28 14:27:30 -0500

    Trim test ns

--- test/keg/core_test.clj
+++ test/keg/core_test.clj
@@ -1,7 +1,1 @@
-(ns keg.core-test
-  (:require [keg.core :as sut]
-            [clojure.test :refer [deftest is testing]]))
-
-(deftest sample-test
-  (testing "I don't test anything useful!"
-    (is (= 1 1))))
+(ns keg.core-test)
