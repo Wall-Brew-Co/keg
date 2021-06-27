3ac89b1e091a141a8de49d0ea2529935103d7245
nnichols <nnichols@users.noreply.github.com>
2021-06-27 18:10:48 +0000

    [Format] Auto-formatting

--- test/keg/core_test.clj
+++ test/keg/core_test.clj
@@ -2,6 +2,7 @@
   (:require [keg.core :as sut]
             [clojure.test :as t]))
 
+
 (t/deftest formatters-test
   (t/is (= {:function-name #'clojure.core/rest :runtime 12345}
            (sut/pour-runtime #'rest 12345 '(2 3 4 5) [1 2 3 4 5])))
--- test/keg/impl_test.clj
+++ test/keg/impl_test.clj
@@ -2,5 +2,6 @@
   (:require [keg.impl :as sut]
             [clojure.test :as t]))
 
+
 (t/deftest name-path->qualified-name-test
   (t/is (= "clojure.core/rest" (sut/name-path->qualified-name ["clojure" "core" "rest"]))))
