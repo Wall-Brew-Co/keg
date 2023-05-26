6edb5e0953cd09d64ea9af0964b89bc91a651d20
Nick A Nichols <nichols1991@gmail.com>
2023-05-26 08:16:10 -0500

    Move CHANGELOG management to sealog (#53)

--- project.clj
+++ project.clj
@@ -1,11 +1,20 @@
 (defproject com.wallbrew/keg "1.1.0"
-  :description "A Clojure library built to tap functions and pour data"
+  :description "A Clojure library built to tap functions to pour runtime data"
   :url "https://github.com/Wall-Brew-Co/keg"
-  :license {:name "MIT"
-            :url  "https://opensource.org/licenses/MIT"}
+  :license {:name         "MIT"
+            :url          "https://opensource.org/licenses/MIT"
+            :distribution :repo
+            :comments     "Same-as all Wall-Brew projects."}
+  :scm {:name "git"
+        :url  "https://github.com/Wall-Brew-Co/keg"}
   :dependencies [[org.clojure/clojure "1.11.1"]
                  [org.clojure/tools.logging "1.2.4"]
                  [metrics-clojure "2.10.0"]
                  [robert/hooke "1.3.0"]]
+  :plugins [[com.github.clj-kondo/lein-clj-kondo "0.2.5"]
+            [com.wallbrew/lein-sealog "1.0.2"]
+            [lein-project-version "0.1.0"]
+            [mvxcvi/cljstyle "0.15.0"]
+            [ns-sort "1.0.3"]]
   :profiles {:uberjar {:aot :all}}
   :min-lein-version "2.5.3")
